d33ede9a94f1077262f98cb28ec400d6f73ae2f4
Ricardo Jesus <ricojesus@hotmail.com>
2018-06-19 13:35:39 -0300

    Inclusão da view para consulta de carros no patio

--- classes/views/movimento_entrada.php
+++ classes/views/movimento_entrada.php
@@ -61,8 +61,8 @@
                 </div>
 
                 <div class="card-footer">
-                <button type="submit" name="btnGravar" class="btn btn-sm btn-primary">Registrar Entrada</button>
-                <button type="submit" name="btnListaMovimento" class="btn btn-sm btn-success">Veículos no Patio</button>
+                <button type="submit" name="btnGravar" class="btn btn-sm btn-primary">Registrar Entrada</button>                
+                <a href="/estacionamento/classes/controllers/movimento_controller.php?action=lista_ativos" class="btn btn-sm btn-success">Veículos no Patio</a>
                 <a href="/estacionamento/" class="btn btn-outline-secondary btn-sm">Voltar</a>
                 </div>            
             </form>
---
+++ classes/views/movimento_lista_ativos.php
@@ -0,0 +1,59 @@
+<br>
+    <!-- Main content -->
+    <form role="form" action="">
+    <section class="content">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-11">
+          <div class="card card-primary card-outline">
+            <!-- /.card-header -->
+            <div class="card-header">
+              <h4 class="card-title">Veículos no Patio</h4>
+            </div>
+            <!-- /.card-header -->
+            <!-- card-body -->             
+            <div class="card-body">
+              <table id="table1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Placa</th>
+                  <th>Marca</th>
+                  <th>Modelo</th>
+                  <th>Entrada</th>
+                  <th width="30px">Ação</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  /*                  
+                  foreach ($lista as $row){
+                    echo '<tr>';
+                    echo '<td>' . $row["descricao"] . '</td>';
+                    echo '<td align="center" >';
+                    echo '<a href="../controllers/teste_controller.php?action=editar&id=' . $row["id"] .  '" class="btn-sm btn-success fa fa-edit" class="confirmation" >Editar</a>';
+                    echo '</tr>';
+                  }
+                  */
+                  ?>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+              <a href="/estacionamento/classes/controllers/movimento_controller.php" class="btn btn-primary">Novo</a>
+            </div>             
+          </div>
+        </div>
+      </div>
+
+    </section>
+    </form>
+    <!-- Main content -->
+
+<script type="text/javascript">
+  $('#table1').DataTable( {
+    responsive: true
+} );
+</script>
+
+<!-- page script -->
