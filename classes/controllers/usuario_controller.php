b62b26ac16430088b08d77d570e102c4f49434af
Ricardo Jesus <ricojesus@hotmail.com>
2018-06-19 10:39:18 -0300

    inclusão do controller de usuários

---
+++ classes/controllers/usuario_controller.php
@@ -0,0 +1,41 @@
+<?php
+include_once ("../views/header.php");
+//include_once ("../models/usuario.php");
+
+//Somente entrará nesse if quando retornando da tela de edição
+if (isset($_POST["btnGravar"])){
+    $usuario = new usuario();
+
+    $usuario->id = $_POST["txtId"];
+    $usuario->nome = $_POST["txtNome"];
+
+    $usuario->save();
+}
+
+//Roteia para a tela correta
+if (isset($_GET["action"])){
+
+    //$usuario = new usuario();
+
+    //if (isset($_GET["id"])){
+    //    $usuario = new usuario($_GET["id"]);
+    //}
+
+    if ($_GET["action"] == "novo"){
+        include_once ("../views/usuario_editar.php");
+    } elseif($_GET["action"] == "editar") {
+        include_once ("../views/usuario_editar.php");
+    }
+} else {
+    $lista[] = ""; // Remover depois de criar a classe usuario
+    //$lista = (new usuario())->list();
+    include_once ("../views/usuario_listar.php"); 
+}
+
+
+// Inclui o Rodape na tela
+include_once ("../views/footer.php");
+
+?>
+
+
