cea17d9bf0db336ccc06d52f44696cadd692b6d7
Ricardo Jesus <ricojesus@hotmail.com>
2018-06-19 14:32:22 -0300

    uso de case na classe Movimento

--- classes/controllers/movimento_controller.php
+++ classes/controllers/movimento_controller.php
@@ -3,13 +3,13 @@ include_once ("../views/header.php");
 include_once ("../models/marca.php");
 
 if (isset($_GET["action"])){
-    if ($_GET["action"] == "saida"){
-        include_once ("../views/movimento_saida.php");
-    }
-    if ($_GET["action"] == "lista_ativos"){
-        include_once ("../views/movimento_lista_ativos.php");
-    }
+    switch ($_GET["action"]){
+        case "saida":
+            include_once ("../views/movimento_saida.php");
+        case "lista_ativos":
+            include_once ("../views/movimento_lista_ativos.php");
 
+    };
 } else {
     $lstMarca = (new Marca())->list();
     include_once ("../views/movimento_entrada.php"); 
@@ -18,5 +18,3 @@ if (isset($_GET["action"])){
 include_once ("../views/footer.php");
 
 ?>
-
-
