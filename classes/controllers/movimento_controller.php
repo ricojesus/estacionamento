d8c02a78ef5f2b52c18fa0f4177aec2dee3525f2
Ricardo Jesus <ricojesus@hotmail.com>
2018-06-19 13:36:23 -0300

    chamada da consulta de carros no patio

--- classes/controllers/movimento_controller.php
+++ classes/controllers/movimento_controller.php
@@ -6,6 +6,10 @@ if (isset($_GET["action"])){
     if ($_GET["action"] == "saida"){
         include_once ("../views/movimento_saida.php");
     }
+    if ($_GET["action"] == "lista_ativos"){
+        include_once ("../views/movimento_lista_ativos.php");
+    }
+
 } else {
     $lstMarca = (new Marca())->list();
     include_once ("../views/movimento_entrada.php"); 
