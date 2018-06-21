174726b5148dea060046ccca041750dff8c28e85
MarlonyTeles <marlonyteles@gmail.com>
2018-06-21 20:00:13 -0300

    Update no preco.php

--- classes/models/preco.php
+++ classes/models/preco.php
@@ -30,6 +30,30 @@ class Preco {
 		return $this->get();
 	}
 
+	//Grava os dados enviados pelo controlador
+	public function save(){
+		$sql = new Sql();
+
+		if ($this->id == 0){
+			$sql->query("INSERT INTO preco (minutos, valor) values (:TEMPO, :VALOR)", array(
+				':TEMPO' => $this->tempo,
+				':VALOR' => $this->valor
+			));
+		} else {
+			$sql->query("UPDATE preco SET minutos=:TEMPO WHERE preco_id = :ID", array(
+				':ID' => $this->id,
+				':TEMPO' => $this->tempo
+			));
+		}		
+	}
+
+	// Exclui logicamente o registro
+	public function delete(){
+		$sql = new Sql();
+
+		$sql->query("UPDATE preco SET status=2 WHERE preco_id = " . $this->id);
+	}
+
 
 	// metodo interno serve para montar todas as consultas de acesso aos dados de teste
 	private function get($criteria = null){
