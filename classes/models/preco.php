5c70f009ccc0b222feeff6ca360cd0651ff9d392
MarlonyTeles <marlonyteles@gmail.com>
2018-06-21 19:34:54 -0300

    Inclusão com ERROS
    Fiz a inclusao com erros (nao consigo achar o a definiçao da função indefinida)

---
+++ classes/models/preco.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once ("../dal/Sql.php");
+
+class Preco {
+
+	//propriedades 
+	public $id; 
+	public $tempo;
+	public $valor; 
+
+	//construtor
+	public function __construct($id = 0){
+		if ($id == 0) {
+			$this->id = 0;
+			$this->tempo = "";
+			$this->valor = "";
+		} else {
+			$result = $this->get("teste_id = ". $id);
+			if (count($result) > 0){
+				$this->id = $result[0]["id"];
+				$this->tempo = $result[0]["tempo"];
+				$this->valor = $result[0]["valor"];
+			}
+		}
+	}
+
+	// Retornar todos os registros cadastrados, serve para carregar a tela de consulta
+	//public function list(){
+	//	return $this->get();
+	//}
+
+
+}
