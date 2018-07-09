383fd0e2c6f7706632b0aed6606bfbb9aa34bbb4
osvaldoneris <osvaldoneris@gmail.com>
2018-07-08 21:07:55 -0300

    Atualização na tela Entrada de Veiculo. Modelo

---
+++ movimento_lista_ativos.php
@@ -0,0 +1,63 @@
+<br>
+    <!-- Main content -->
+    <form role="form" action="">
+    <section class="content">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-11">
+          <div class="card card-primary card-outline">
+            <!-- /.card-header -->
+            <div class="card-header">
+              <h4 class="card-title">Veículos no Patio</h4>
+            </div>
+            <!-- /.card-header -->
+            <!-- card-body -->             
+            <div class="card-body">
+              <table id="table1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Placa</th>
+                  <th>Marca</th>
+                  <th>Modelo</th>
+                  <th>Entrada</th>
+                  <th width="30px">Ação</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  //a lista deve ser criada dentro do controller
+                  //$lista = (new Movimento())->listActives();        
+                  foreach ($lista as $row){
+                    echo '<tr>';
+                    echo '<td>' . $row["placa"] . '</td>';
+                    echo '<td>' . $row["marca"] . '</td>';
+                    echo '<td>' . $row["modelo"] . '</td>';
+                    echo '<td>' . $row["entrada"] . '</td>';
+                    echo '<td align="center" >';
+                    echo '<a href="../controllers/movimento_controller.php?action=saida&id=' . $row["id"] .  '" class="btn-sm btn-success fa fa-edit" class="confirmation" >Saída</a>';
+                    echo '</tr>';
+                  }
+                 
+                  ?>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+              <a href="/estacionamento/classes/controllers/movimento_controller.php" class="btn btn-primary">Novo</a>
+            </div>             
+          </div>
+        </div>
+      </div>
+
+    </section>
+    </form>
+    <!-- Main content -->
+
+<script type="text/javascript">
+  $('#table1').DataTable( {
+    responsive: true
+} );
+</script>
+
+<!-- page script -->
